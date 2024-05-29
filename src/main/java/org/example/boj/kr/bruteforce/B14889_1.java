a04c462a2dd87f7c707e3dd8d383b59aa53dfa8a
thswlsqls <thswlsqls@naver.com>
2024-05-29 22:16:17 +0900

    commit changes

--- src/main/java/org/example/boj/kr/bruteforce/B14889_1.java
+++ src/main/java/org/example/boj/kr/bruteforce/B14889_1.java
@@ -4,8 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B14889 {
-    static int N = 0;
+public class B14889_1 {
+
+    static int N;
     static int minDiff = Integer.MAX_VALUE;
     static int[][] board;
     static boolean[] visited;
@@ -16,7 +17,7 @@ public class B14889 {
             N = Integer.parseInt(br.readLine());
             board = new int[N][N];
             for (int r=0; r<N; r++) {
-                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int c=0; c<N; c++) {
                     board[r][c] = Integer.parseInt(st.nextToken());
                 }
@@ -30,12 +31,12 @@ public class B14889 {
     }
 
     public static void combination(int start, int count) {
-        if (count == N/2) {
+        if (count==N/2){
             setMinDiff();
             return;
         }
-        for (int i = start; i<N; i++) {
-            if(!visited[i]) {
+        for (int i=start; i<N; i++) {
+            if (!visited[i]){
                 visited[i] = true;
                 combination(i+1, count+1);
                 visited[i] = false;
@@ -46,14 +47,14 @@ public class B14889 {
     public static void setMinDiff() {
         int start = 0;
         int link = 0;
-        for (int r=0; r<N; r++) {
-            for (int c=r+1; c<N; c++) {
-                if (r == c) continue;
-                if (visited[r] && visited[c]){
+        for (int r=0; r<N; r++){
+            for (int c=r+1; c<N; c++){
+                if (r==c) continue;
+                if(visited[r] && visited[c]){
                     start += board[r][c];
                     start += board[c][r];
                 }
-                if (!visited[r] && !visited[c]){
+                if(!visited[r] && !visited[c]){
                     link += board[r][c];
                     link += board[c][r];
                 }
@@ -61,9 +62,8 @@ public class B14889 {
         }
         minDiff = Math.min(minDiff, Math.abs(start-link));
         if (minDiff == 0){
-            System.out.println(minDiff);
+            System.out.println(0);
             System.exit(0);
         }
     }
-
 }
